73888131401637f9c423e45afe9ead44874b06e2
Zorawar Singh Mahal <118074455+zorawarMahal@users.noreply.github.com>
2025-09-12 23:16:24 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://algo-bull-ecru.vercel.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
